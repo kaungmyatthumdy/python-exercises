2128109cdf5a49a48306b58a27abc49de19b2192
kaungmyatthumdy <khathtan3131997@gmail.com>
2018-06-26 12:46:42 +0630

    change from dog to cat

--- ex101.py
+++ ex101.py
@@ -1,17 +1,17 @@
 
-tabby_dog="\t I'm tabled in."
-persian_dog="I'm split\non a line."
-backslash_dog= "I'm \\ a \\dog."
-fat_dog="""
+tabby_cat="\t I'm tabled in."
+persian_cat="I'm split\non a line."
+backslash_cat= "I'm \\ a \\cat."
+fat_cat="""
 I'll do a list:
-    \t* dog Food
+    \t* cat Food
     \t*Finishies
     \t* Catnip \n \t* Grass
     """
-print(tabby_dog)
-print(persian_dog)
-print(backslash_dog)
-print(fat_dog)
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
 
 
 
